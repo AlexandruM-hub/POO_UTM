a3ce0a0130b9e09f321c42dfd80d3bd8c6997721
Mazureac Alexandru <71084445+AlexandruM-hub@users.noreply.github.com>
2021-09-27 19:45:13 +0300

    Add files via upload

---
+++ Lab.4/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String test = "Literary critics, historians, avid readers, and even casual readers will all have different opinions on which novel is truly the “greatest book ever written.” Is it a novel with beautiful, captivating figurative language? Or one with gritty realism? A novel that has had an immense social impact? Or one that has more subtly affected the world? Here is a list of 12 novels that, for various reasons, have been considered some of the greatest works of literature ever written.";
+        Manipulation a = new Manipulation();
+        a.NumberOf(test);
+        a.topWords(test);
+    }
+}
